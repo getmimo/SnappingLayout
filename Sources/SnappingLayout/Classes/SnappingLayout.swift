6aac568b3a45a8801363ec7f5ec24963aaaca5b7
Lukas Carvajal <lukascarvajal@gmail.com>
2021-05-13 01:05:12 +0200

    Add support for .top snapping for vertical scrolling

--- Sources/SnappingLayout/Classes/SnappingLayout.swift
+++ Sources/SnappingLayout/Classes/SnappingLayout.swift
@@ -13,6 +13,7 @@ public class SnappingLayout: UICollectionViewFlowLayout {
         case left
         case center
         case right
+        case top
     }
 
     // MARK: - Properties
@@ -29,52 +30,113 @@ public class SnappingLayout: UICollectionViewFlowLayout {
         guard let collectionView = collectionView else {
             return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
         }
-
+        
         var offsetAdjusment = CGFloat.greatestFiniteMagnitude
         let horizontalPosition: CGFloat
-
-        switch snapPosition {
-        case .left:
-            horizontalPosition = proposedContentOffset.x + collectionView.contentInset.left + sectionInset.left
-        case .center:
-            horizontalPosition = proposedContentOffset.x + (collectionView.bounds.width * 0.5)
-        case .right:
-            horizontalPosition = proposedContentOffset.x + collectionView.bounds.width - sectionInset.right
-        }
-
-        let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
-        let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
-        layoutAttributesArray?.forEach { layoutAttributes in
-            let itemHorizontalPosition: CGFloat
-
+        let verticalPosition: CGFloat
+        
+        switch scrollDirection {
+        case .horizontal:
             switch snapPosition {
             case .left:
-                itemHorizontalPosition = layoutAttributes.frame.minX - collectionView.contentInset.left
+                horizontalPosition = proposedContentOffset.x + collectionView.contentInset.left + sectionInset.left
+                verticalPosition = proposedContentOffset.y
             case .center:
-                itemHorizontalPosition = layoutAttributes.center.x
+                horizontalPosition = proposedContentOffset.x + (collectionView.bounds.width * 0.5)
+                verticalPosition = proposedContentOffset.y
             case .right:
-                itemHorizontalPosition = layoutAttributes.frame.maxX + collectionView.contentInset.right
+                horizontalPosition = proposedContentOffset.x + collectionView.bounds.width - sectionInset.right
+                verticalPosition = proposedContentOffset.y
+            case .top:
+                horizontalPosition = proposedContentOffset.x
+                verticalPosition = proposedContentOffset.y
             }
-
-            if abs(itemHorizontalPosition - horizontalPosition) < abs(offsetAdjusment) {
-                // If the drag velocity is lower than the minimum velocity (no matter the direction):
-                // snap the current cell to it's original position.
-                if abs(velocity.x) < self.minimumSnapVelocity {
-                    offsetAdjusment = itemHorizontalPosition - horizontalPosition
-                }
-                // If the velocity is higher than the snap threshold and drag is right->left:
-                // move to the next cell on the right.
-                else if velocity.x > 0 {
-                    offsetAdjusment = itemHorizontalPosition - horizontalPosition + (layoutAttributes.bounds.width + self.minimumLineSpacing)
-                }
-                // If the velocity is higher than the snap threshold and drag is left->right:
-                // move to the next cell on the left.
-                else { // velocity.x < 0
-                    offsetAdjusment = itemHorizontalPosition - horizontalPosition - (layoutAttributes.bounds.width + self.minimumLineSpacing)
-                }
+        case .vertical:
+            switch snapPosition {
+            case .left, .right:
+                horizontalPosition = proposedContentOffset.x
+                verticalPosition = proposedContentOffset.y
+            case .center:
+                horizontalPosition = proposedContentOffset.x
+                verticalPosition = proposedContentOffset.y
+            case .top:
+                horizontalPosition = proposedContentOffset.x
+                verticalPosition = proposedContentOffset.y + collectionView.bounds.height - sectionInset.top
             }
         }
+        
+        let targetRect = CGRect(x: proposedContentOffset.x, y: proposedContentOffset.y, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
+        let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
+        layoutAttributesArray?.forEach { layoutAttributes in
+            
+            switch scrollDirection {
+            case .horizontal:
+                let itemHorizontalPosition: CGFloat
+                
+                switch snapPosition {
+                case .left:
+                    itemHorizontalPosition = layoutAttributes.frame.minX - collectionView.contentInset.left
+                case .center:
+                    itemHorizontalPosition = layoutAttributes.center.x
+                case .right:
+                    itemHorizontalPosition = layoutAttributes.frame.maxX + collectionView.contentInset.right
+                case .top:
+                    itemHorizontalPosition = horizontalPosition
+                }
+                
+                if abs(itemHorizontalPosition - horizontalPosition) < abs(offsetAdjusment) {
+                    // If the drag velocity is lower than the minimum velocity (no matter the direction):
+                    // snap the current cell to it's original position.
+                    if abs(velocity.x) < self.minimumSnapVelocity {
+                        offsetAdjusment = itemHorizontalPosition - horizontalPosition
+                    }
+                    // If the velocity is higher than the snap threshold and drag is right->left:
+                    // move to the next cell on the right.
+                    else if velocity.x > 0 {
+                        offsetAdjusment = itemHorizontalPosition - horizontalPosition + (layoutAttributes.bounds.width + self.minimumLineSpacing)
+                    }
+                    // If the velocity is higher than the snap threshold and drag is left->right:
+                    // move to the next cell on the left.
+                    else { // velocity.x < 0
+                        offsetAdjusment = itemHorizontalPosition - horizontalPosition - (layoutAttributes.bounds.width + self.minimumLineSpacing)
+                    }
+                }
+            case .vertical:
+                let itemVerticalPosition: CGFloat
 
-        return CGPoint(x: proposedContentOffset.x + offsetAdjusment, y: proposedContentOffset.y)
+                switch snapPosition {
+                case .left:
+                    itemVerticalPosition = verticalPosition
+                case .center:
+                    itemVerticalPosition = verticalPosition
+                case .right:
+                    itemVerticalPosition = verticalPosition
+                case .top:
+                    itemVerticalPosition = layoutAttributes.frame.minY - collectionView.contentInset.top
+                }
+                
+                if abs(itemVerticalPosition - verticalPosition) < abs(offsetAdjusment) {
+                    // If the drag velocity is lower than the minimum velocity (no matter the direction):
+                    // snap the current cell to it's original position.
+                    if abs(velocity.y) < self.minimumSnapVelocity {
+                        offsetAdjusment = itemVerticalPosition - verticalPosition
+                    }
+                    // If the velocity is higher than the snap threshold and drag is right->left:
+                    // move to the next cell on the right.
+                    else if velocity.y > 0 {
+                        offsetAdjusment = itemVerticalPosition - verticalPosition + (layoutAttributes.bounds.height + self.minimumLineSpacing)
+                    }
+                    // If the velocity is higher than the snap threshold and drag is left->right:
+                    // move to the next cell on the left.
+                    else { // velocity.x < 0
+                        offsetAdjusment = itemVerticalPosition - verticalPosition - (layoutAttributes.bounds.height + self.minimumLineSpacing)
+                    }
+                }
+            }
+            
+            
+        }
+
+        return CGPoint(x: proposedContentOffset.x + offsetAdjusment, y: proposedContentOffset.y + offsetAdjusment)
     }
 }
